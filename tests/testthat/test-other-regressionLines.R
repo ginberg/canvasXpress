6011443d06b7f28cebe0190d92cbefeb7a9f913d
ginberg <ginberg@gmail.com>
2018-10-25 21:26:40 +0200

    update regression lines test

--- tests/testthat/test-other-regressionLines.R
+++ tests/testthat/test-other-regressionLines.R
@@ -17,38 +17,28 @@ test_that("scatterplot overall regressionLine", {
 
 test_that("scatterplot per-sample regressionLine", {
 
+    y=read.table("http://www.canvasxpress.org/data/cX-mtcars-dat.txt", header=TRUE, sep="\t", quote="", row.names=1, fill=TRUE, check.names=FALSE, stringsAsFactors=FALSE)
     result <-  canvasXpress(
         data                    = y,
-        smpAnnot                = x,
+        asSampleFactors         = list("cyl"),
+        colorBy                 = "cyl",
         graphType               = "Scatter2D",
-        title                   = "Scatterplot - regressionLine",
-        afterRender             = list(list("addRegressionLine"))
+        title                   = "Scatterplot - per sample regressionLine",
+        showRegressionFullRange = TRUE,
+        stringVariableFactors   = list("cyl"),
+        xAxis                   = list("wt"),
+        yAxis                   = list("mpg"),
+        afterRender             = list(list("addRegressionLine", list("cyl")))
     )
     check_ui_test(result)
 })
 
 test_that("scatterplot per variable regressionLine", {
-
-    result <-  canvasXpress(
-        data                    = y,
-        smpAnnot                = x,
-        graphType               = "Scatter2D",
-        title                   = "Scatterplot - regressionLine",
-        afterRender             = list(list("addRegressionLine"))
-    )
-    check_ui_test(result)
+    ## TODO
 })
 
 # segregate the chart per sample and variable and have a regression line for each
 test_that("scatterplot per variable and sample regressionLine", {
-
-    result <-  canvasXpress(
-        data                    = y,
-        smpAnnot                = x,
-        graphType               = "Scatter2D",
-        title                   = "Scatterplot - regressionLine",
-        afterRender             = list(list("addRegressionLine"))
-    )
-    check_ui_test(result)
+    ## TODO
 })
 
