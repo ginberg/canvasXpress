f9b33fc9b25b99dc771473b0ab335c07aacf1716
Ger Inberg <gerinberg@ip-10-0-0-240.ca-central-1.compute.internal>
2018-11-06 06:28:08 +0000

    update resizable test

--- tests/testthat/test-other-resizable.R
+++ tests/testthat/test-other-resizable.R
@@ -1,8 +1,10 @@
 context("canvasXpress resizable")
 
+
+y <- read.table("http://www.canvasxpress.org/data/cX-mtcars-dat.txt", header = TRUE, sep = "\t", quote = "", row.names = 1, fill = TRUE, check.names = FALSE, stringsAsFactors = FALSE)
+
 test_that("scatterplot resizable", {
 
-    y <- read.table("http://www.canvasxpress.org/data/cX-mtcars-dat.txt", header = TRUE, sep = "\t", quote = "", row.names = 1, fill = TRUE, check.names = FALSE, stringsAsFactors = FALSE)
     result <-  canvasXpress(
         data                    = y,
         asSampleFactors         = list("cyl"),
@@ -14,10 +16,44 @@ test_that("scatterplot resizable", {
     )
     check_ui_test(result)
 
-    warning("Resizing the canvas by dragging the mouse over the edges doesn't work initially (does work after clicking Minimize/Maximize)")
+    warning("Increasing the canvas size by dragging the mouse over the edges doesn't work initially (does work after clicking Minimize/Maximize)")
 })
 
-test_that("barplot not resizable", {
+test_that("scatterplot resizable width", {
+
+    result <-  canvasXpress(
+        data                    = y,
+        asSampleFactors         = list("cyl"),
+        colorBy                 = "cyl",
+        graphType               = "Scatter2D",
+        stringVariableFactors   = list("cyl"),
+        title                   = "Scatterplot - resizable width",
+        resizableX              = TRUE,
+        resizableY              = FALSE
+    )
+    check_ui_test(result)
+
+    warning("Increasing the canvas width by dragging the mouse over the edges doesn't work initially (does work after clicking Minimize/Maximize)")
+})
+
+test_that("scatterplot resizable height", {
+
+    result <-  canvasXpress(
+        data                    = y,
+        asSampleFactors         = list("cyl"),
+        colorBy                 = "cyl",
+        graphType               = "Scatter2D",
+        stringVariableFactors   = list("cyl"),
+        title                   = "Scatterplot - resizable height",
+        resizableY              = TRUE,
+        resizableX              = FALSE
+    )
+    check_ui_test(result)
+
+    warning("Increasing the canvas height by dragging the mouse over the edges doesn't work initially (does work after clicking Minimize/Maximize)")
+})
+
+test_that("scatterplot not resizable", {
 
     y <- read.table(system.file("extdata", "cX-basic-dat.txt", package = "canvasXpress"), header = TRUE, sep = "\t", quote = "", row.names = 1, fill = TRUE, check.names = FALSE, stringsAsFactors = FALSE)
     result <-  canvasXpress(
