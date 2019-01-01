0bb3da862cb646a41e30f12d65d1e077a4b13b87
Ger Inberg <gerinberg@ip-10-0-0-240.ca-central-1.compute.internal>
2019-01-01 04:19:49 +0000

    testing 22.3: add warnings

--- tests/testthat/test-ui-bar.R
+++ tests/testthat/test-ui-bar.R
@@ -7,10 +7,14 @@ test_that("cXbar1", {
 
 test_that("cXbar2", {
     check_ui_test(cXbar2())
+
+    warning("disabling Sample1 bar disables all 'Variable1' bars if 'Variable1' has been disabled/enabled before.")
 })
 
 test_that("cXbar3", {
     check_ui_test(cXbar3())
+
+    warning("disabling Sample1 bar disables all 'Variable1' bars if 'Variable1' has been disabled/enabled before.")
 })
 
 test_that("cXbar4", {
@@ -19,6 +23,8 @@ test_that("cXbar4", {
 
 test_that("cXbar5", {
     check_ui_test(cXbar5())
+
+    warning("disabling Sample1 bar disables all 'Variable1' bars if 'Variable1' has been disabled/enabled before.")
 })
 
 test_that("cXbar6", {
@@ -31,6 +37,8 @@ test_that("cXbar7", {
 
 test_that("cXbar8", {
     check_ui_test(cXbar8())
+
+    warning("disabling Sample1 bar disables all 'Variable1' bars if 'Variable1' has been disabled/enabled before.")
 })
 
 test_that("cXbar9", {
--- tests/testthat/test-ui-barline.R
+++ tests/testthat/test-ui-barline.R
@@ -3,12 +3,18 @@ context("canvasXpress Web Charts - BarLine")
 
 test_that("cXbarline1", {
     check_ui_test(cXbarline1())
+
+    warning("disabling Sample1 bar disables all 'Variable1' bars if 'Variable1' has been disabled/enabled before.")
 })
 
 test_that("cXbarline2", {
     check_ui_test(cXbarline2())
+
+    warning("disabling Sample1 bar disables all 'Variable1' bars if 'Variable1' has been disabled/enabled before.")
 })
 
 test_that("cXbarline3", {
     check_ui_test(cXbarline3())
+
+    warning("disabling Sample1 bar disables all 'Variable1' bars if 'Variable1' has been disabled/enabled before.")
 })
