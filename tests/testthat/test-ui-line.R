7d6e91198603935c2895f470c5ae1cd5882c0de2
Ger <ger@aggregate-genius.com>
2019-06-25 14:11:22 +0000

    24.4 test updates

--- tests/testthat/test-ui-line.R
+++ tests/testthat/test-ui-line.R
@@ -3,10 +3,14 @@ context("canvasXpress Web Charts - Line")
 
 test_that("cXline1", {
     check_ui_test(cXline1())
+
+    warning("when clicking on 'Sample1', the corresponding plot data is not disabled")
 })
 
 test_that("cXline2", {
     check_ui_test(cXline2())
+
+    warning("when clicking on 'Sample1', the corresponding plot data is not disabled")
 })
 
 test_that("cXline3", {
