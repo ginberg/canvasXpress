d96051790722dfbd77f54d29fb7c416105fc3f78
Ger <ger@aggregate-genius.com>
2019-06-25 13:24:47 +0000

    24.4 test updates

--- tests/testthat/test-ui-radar.R
+++ tests/testthat/test-ui-radar.R
@@ -4,7 +4,7 @@ context("canvasXpress Web Charts - Radar")
 test_that("cXradar1", {
     check_ui_test(cXradar1())
 
-    warning("when clicking on 'Sample1', the corresponding data is not disabled")
+    warning("when clicking on 'Sample1', the corresponding data is not disabled/hidden")
 })
 
 test_that("cXradar2", {
@@ -14,35 +14,35 @@ test_that("cXradar2", {
 test_that("cXradar3", {
     check_ui_test(cXradar3())
 
-    warning("when clicking on 'Sample1', the corresponding data is not disabled")
+    warning("when clicking on 'Sample1', the corresponding data is not disabled/hidden")
 })
 
 test_that("cXradar4", {
     check_ui_test(cXradar4())
 
-    warning("when clicking on 'Sample1', the corresponding data is not disabled")
+    warning("when clicking on 'Sample1', the corresponding data is not disabled/hidden")
 })
 
 test_that("cXradar5", {
     check_ui_test(cXradar5())
 
-    warning("when clicking on 'Sample1', the corresponding data is not disabled")
+    warning("when clicking on 'Sample1', the corresponding data is not disabled/hidden")
 })
 
 test_that("cXradar6", {
     check_ui_test(cXradar6())
 
-    warning("when clicking on 'Sample1', the corresponding data is not disabled")
+    warning("when clicking on 'Sample1', the corresponding data is not disabled/hidden")
 })
 
 test_that("cXradar7", {
     check_ui_test(cXradar7())
 
-    warning("when clicking on 'Sample1', the corresponding data is not disabled")
+    warning("when clicking on 'Sample1', the corresponding data is not disabled/hidden")
 })
 
 test_that("cXradar8", {
     check_ui_test(cXradar8())
 
-    warning("when clicking on 'Sample1', the corresponding data is not disabled")
+    warning("when clicking on 'Sample1', the corresponding data is not disabled/hidden")
 })
--- tests/testthat/test-ui-stackedline.R
+++ tests/testthat/test-ui-stackedline.R
@@ -4,11 +4,11 @@ context("canvasXpress Web Charts - StackedLine")
 test_that("cXstackedline1", {
     check_ui_test(cXstackedline1())
 
-    warning("when clicking on 'Sample1', the corresponding data is not disabled")
+    warning("when clicking on 'Sample1', the corresponding data is not disabled/hidden")
 })
 
 test_that("cXstackedline2", {
     check_ui_test(cXstackedline2())
 
-    warning("when clicking on 'Sample1', the corresponding data is not disabled")
+    warning("when clicking on 'Sample1', the corresponding data is not disabled/hidden")
 })
