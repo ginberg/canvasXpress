fa1c51c4a04de93d42477a59562c3b3ae08c134b
Ger <ger@aggregate-genius.com>
2019-07-29 10:41:19 +0000

    test update 1.24.8

--- tests/testthat/test-ui-bar.R
+++ tests/testthat/test-ui-bar.R
@@ -15,6 +15,8 @@ test_that("cXbar3", {
 
 test_that("cXbar4", {
     check_ui_test(cXbar4())
+
+    warning("Inconsistent Tooltip: for Setosa/Sepal.Length dots, it's showing the second number in scientific format")
 })
 
 test_that("cXbar5", {
@@ -41,10 +43,12 @@ test_that("cXbar10", {
     check_ui_test(cXbar10())
 
     warning("Durable overlay looks different in R than on the CX website, missing the following parm: smpOverlayProperties=list('Durable' = list('type'='Boolean'))")
+    warning("When clicking on Durable with value = 0 (e.g. at S8), all bars are disabled")
 })
 
 test_that("cXbar11", {
     check_ui_test(cXbar11())
 
     warning("Durable overlay looks different in R than on the CX website, missing the following parm: smpOverlayProperties=list('Durable' = list('type'='Boolean'))")
+    warning("When clicking on Durable with value = 0 (e.g. at S8), all bars are disabled")
 })
